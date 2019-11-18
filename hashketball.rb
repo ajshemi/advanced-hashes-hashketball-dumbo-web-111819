d349e575aeaf9c34b501d6fe726ede0b14e9a3dd
Akpojotor Shemi <akpojotor@yahoo.com>
2019-11-18 21:48:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code here!
 AA={:player_name =>"Alan Anderson",:number => 0, :shoe => 16,:points => 22,:rebounds => 12,:assists =>12,:steals => 3,:blocks => 1,:slam_dunks => 1}
 RE={:player_name =>"Reggie Evans",:number => 30, :shoe => 14,:points => 12,:rebounds => 12,:assists =>12,:steals => 12,:blocks => 12,:slam_dunks => 7}
@@ -33,6 +34,7 @@ def num_points_scored(name)
   game_data.each_value do |value|
     value[:players].each do |value1|
       if value1[:player_name] == name
+        binding.pry
         result << value1[:points]
       end
     end
